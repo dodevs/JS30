c40f4423eaeea00092fe8fe556168ed678f33cb4
dodevs <douglass.sousa@outlook.com.br>
2018-07-12 17:07:13 -0300

    ArrayCardio Day 1 Incomplete

---
+++ public/js/arrayCardio1.js
@@ -0,0 +1,78 @@
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+  ];
+
+  const people = [
+    'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 
+    'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 
+    'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 
+    'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana',
+    'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 
+    'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 
+    'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 
+    'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose',
+    'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 
+    'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'
+];
+
+//Array.protoype.filter()
+//1. FIlter the list of inventors for those who were born in the 1500's
+var inventorsFiltered = inventors.filter(function(inventor){
+  return inventor.year >= 1500 && inventor.year < 1600;
+})
+
+// Array.prototype.map()
+//2. Give us an array of the inventory first and last names
+var flNames = inventors.map(function(inventor){
+  return `${inventor.first} ${inventor.last}`;
+  //return Array.of(inventor.first, inventor.last);
+})
+
+// Array.prototype.sort
+// 3. Sort the inventors by birthdate, oldest to youngest
+var sortedInventors = inventors.sort(function(inventorA, inventorB){
+  if(inventorA.year < inventorB.year){
+    return -1;
+  }else if(inventorA.year > inventorB.year){
+    return 1;
+  }else{
+    return 0;
+  }
+})
+
+//Array.prototype.reduce
+//4 . How many years did all the inventors live?
+var inventorYS = inventors.reduce(function(accum, inventor){
+  return accum + (inventor.passed - inventor.year);
+}, 0);//0 é o valor inicial da soma
+
+//5. Sort the inventors by years lived
+var livedSort = inventors.sort(function(iA, iB){
+  if( (iA.passed - iA.year) < (iB.passed - iB.year)){
+    return -1;
+  }else if((iA.passed - iA.year) > (iB.passed - iB.year)){
+    return 1;
+  }else{
+    return 0;
+  }
+});
+
+//6. Create a list of Boulevards in paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+console.table(inventorsFiltered);
+console.table(flNames);
+console.table(sortedInventors);
+console.log(inventorYS);
+console.table(livedSort);
